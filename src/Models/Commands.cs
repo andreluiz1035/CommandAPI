4d4aa00e72c370ba7fa34fc21bc6ac929efafeb4
andreluiz1035 <andreluiz.mcse@gmail.com>
2020-06-12 14:37:58 -0300

    Added Command Model to API Project

---
+++ src/Models/Commands.cs
@@ -0,0 +1,13 @@
+
+
+namespace CommanndAPI.Models
+{
+    public class Command
+    {
+gi        public int Id {get;set;}
+        public string HowTo {get;set;}
+        public string Plataform {get;set;}
+        public string CommandLine {get;set;}
+    }
+
+}
